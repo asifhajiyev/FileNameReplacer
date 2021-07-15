090b46d1f032138397b9f7ef7e816e542f7912ca
Asif Hajiyev <asif.haciyev@kapitalbank.az>
2021-07-15 10:46:29 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String path = "C:\\Users\\HaciyevAB\\source\\repos\\cca-3cx-new\\Audio\\mainMenuBalance\\numbers";
+        String replaceFrom = "en";
+        String replaceTo = "eng";
+        replaceFileName(path, replaceFrom, replaceTo);
+    }
+
+    private static void replaceFileName(String path, String replaceFrom, String replaceTo) {
+        File thisFolder = new File(path);
+        File[] filesArray = thisFolder.listFiles();
+
+        for (File file : filesArray) {
+            if (file.getName().contains(replaceFrom)) {
+                String oldName = file.getName();
+                file.renameTo(new File(thisFolder, oldName.replace(replaceFrom, replaceTo)));
+            }
+        }
+    }
+}
